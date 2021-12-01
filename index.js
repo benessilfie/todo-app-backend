26ef71c9d1d85efa36d2ae0fc43e2329e7f4f422
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2021-12-01 15:00:27 +0000

    Initial commit of the practice backend server

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+//created an instace of the express server
+const app = express();
+app.use(express.json());
+
+
+const PORT = process.env.PORT?? 6800;
+
+app.get("/", (req, res)=>{
+    res.send("Hello World!, This is Practice");
+});
+
+//Connect to MongoAtlas Database#
+mongoose.connect(process.env.MONGO_DB_PRACTICE, (error) => {
+    if (error) {
+        return console.log("Connection to MongoDB was unsuccesful");
+    }
+    else {
+        console.log("Connection to MongoDB was succesful");
+        app.listen(PORT, ()=>console.log(`Practice server is up and running on ${PORT}`));
+    }
+} )
+
+
+
+
+
