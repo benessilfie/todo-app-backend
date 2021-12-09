ce585285ac2c60a13263152cd0325d5835f8d661
Benjamin Ofori-Quansah <bquansah007@gmail.com>
2021-12-09 16:39:00 +0000

    created models

--- models/todo.js
+++ models/todo.js
@@ -4,14 +4,16 @@ import mangoose from "mongoose";
 //unpack schema & model from mongoose
 const {schema, model} = mongoose;
 
-// the schema defines the structure of our collection (Table) in MongoDB
-//the model defines how data will modeled for our collection
-//and comes along with built in features to manipulate the data
+// The schema defines the structure of our collection (Table) in MongoDB the model defines how data will modeled
+//for our collection and comes along with built in features to manipulate the data
 
-//create a new schema instance specifying which fields (columns) you want in the collection (Table)
+//Create a new schema instance specifying which fields (columns) you want in the collection (Table)
 const todoSchema = Schema({
     title: String,
     description: String,
     dateTime: String,
     isCompleted: Boolean
 });
+
+// Make a model by passing in the name and a schema for our model
+export const Todo = model('todo',todoSchema );
